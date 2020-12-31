316b2e769cd1074081808b5f84213335f32b265e
adamtlee <adam.lee346@gmail.com>
2020-12-31 12:43:31 -0700

    Added Customer Repository

---
+++ ACM/CustomerRepository.cs
@@ -0,0 +1,37 @@
+﻿using ACM.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM
+{
+    public class CustomerRepository
+    {
+        public Customer Retrieve(int customerId)
+        {
+            // Create an instance of the customer class
+            // Pass in the requested id
+            Customer customer = new Customer(customerId); 
+
+            // Code to retrieve the defined customer
+
+            // Temp hard coded values to return populated customer
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "hpotter@mail.com";
+                customer.FirstName = "harry";
+                customer.LastName = "potter";
+            }
+
+            return customer;
+        }
+
+        public bool Save(Customer customer)
+        {
+            // Code to save passed in customer
+            return true;
+        }
+    }
+}
