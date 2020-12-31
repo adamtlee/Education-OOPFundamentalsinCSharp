4b22f8ce07f61283c7a777c5ef4f103a031eb471
adamtlee <adam.lee346@gmail.com>
2020-12-31 13:15:12 -0700

    Removed Order Methods

--- ACM/Order.cs
+++ ACM/Order.cs
@@ -20,16 +20,6 @@ namespace ACM
         public int OrderId { get; private set; }
         public DateTimeOffset? OrderDate { get; set; }
 
-        public bool Save()
-        {
-            return true;
-        }
-
-        public Order Retrieve(int orderId)
-        {
-            return new Order();
-        }
-
         public bool Validate()
         {
             var isValid = true;
