e22ee258ab3e1e620bf56287ffea09fb8dfbda34
adamtlee <adam.lee346@gmail.com>
2021-01-09 16:47:07 -0700

    Implimented Interface based polymorphism

---
+++ Tests/Acme.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using ACM;
+using ACM.BL;
+using Acme.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.CommonTest
+{
+    /// <summary>
+    /// Summary description for LoggingServiceTest
+    /// </summary>
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                EmailAddress = "bob@mail.com",
+                FirstName = "bob",
+                LastName = "ross",
+                AddressList = null
+            };
+            changedItems.Add(customer);
+
+            var product = new Product(2)
+            {
+                ProductName = "Sour Gummies",
+                ProductDescription = "Some Sour Candy",
+                CurrentPrice = 5M
+            };
+            changedItems.Add(product);
+
+            LoggingService.WriteToFile(changedItems);
+        }
+    }
+}
