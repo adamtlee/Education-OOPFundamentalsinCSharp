8041cb4ca2cbbccc8eeab9e244ba1fdb64a8cea0
adamtlee <adam.lee346@gmail.com>
2021-01-09 14:11:19 -0700

    StringHandler and test for StringHandler

---
+++ Tests/Acme.CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Acme.CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acme.CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e760e15d-2d97-4ac5-8ae0-bb4c6c9d7371")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Acme.CommonTest/StringHandlerTest.cs
@@ -0,0 +1,34 @@
+﻿using Acme.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class StringHandlerTest
+    {
+        [TestMethod]
+        public void InsertSpacesTestValid()
+        {
+            var source = "BobRoss";
+            var expected = "Bob Ross";
+            var handler = new StringHandler();
+
+            var actual = handler.InsertSpaces(source);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertSpacesTEsWithExistingSpace()
+        {
+            var source = "Bob Ross";
+            var expected = "Bob Ross";
+            var handler = new StringHandler();
+
+            var actual = handler.InsertSpaces(source);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
